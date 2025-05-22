674c69557d6e550a32c0e7a44aa5686bb87fc786
Denariadna <149600221+Denariadna@users.noreply.github.com>
2025-05-22 22:48:31 +0300

    add components: user-list, user-details, user-form

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import {
+  PlusOutline,
+  SaveOutline,
+  EditOutline,
+  EyeOutline,
+  ArrowLeftOutline,
+} from '@ant-design/icons-angular/icons';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export const icons = [
+  PlusOutline,
+  SaveOutline,
+  EditOutline,
+  EyeOutline,
+  ArrowLeftOutline,
+];
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [...appConfig.providers, { provide: NZ_ICONS, useValue: icons }],
+});
